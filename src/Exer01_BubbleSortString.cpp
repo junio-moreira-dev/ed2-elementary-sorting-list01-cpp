eb619e668c8ac11267bbdd8f6b2e36b0d0a67c00
junio-moreira <dispmob.ads.iftm@gmail.com>
2026-08-19 21:05:32 -0300

    feat: add template and test suite for Exercise 01

--- src/Exer01_BubbleSortString.cpp
+++ src/Exer01_BubbleSortString.cpp
@@ -1,3 +1,56 @@
 //
 // Created by Juninho Moreira on 18/08/26.
 //
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+
+//#include <bits/stdc++.h>
+
+using namespace std;
+
+#define IOFAST() ios_base::sync_with_stdio(0);cin.tie(0);
+#define ulli unsigned long long int
+#define uli unsigned long int
+#define lli long long int
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef pair<ll,ll> ii;
+typedef vector<ll> vi;
+typedef vector< ii > vii;
+
+#define INF 0x3F3F3F3F
+#define LINF 0x3F3F3F3F3F3F3F3FLL
+#define pb push_back
+#define mp make_pair
+#define pq priority_queue
+#define LSONE(s) ((s)&(-s)) //LASTBIT
+#define DEG_to_RAD(X)   (X * PI / 180)
+#define F first
+#define S second
+#define PI 2*acos(0)
+
+// 1. PURE LOGIC
+// TODO: Definir a funcao principal de solucao
+
+// 2. ONLINE JUDGE ADAPTER
+void onlineJudge() {
+    // TODO: Leitura de entradas e chamada da solucao para juiz online
+}
+
+// 3. LOCAL TESTS
+void runLocalTests() {
+    // TODO: Implementar assercoes e casos de teste locais
+}
+
+int main() {
+    IOFAST();
+    runLocalTests();  // <- modo de testes locais
+    // onlineJudge(); // <- modo juiz online
+
+    return 0;
+}
